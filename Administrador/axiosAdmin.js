843e1e9b460cf2dbde3f2c02b65ed049ec34131d
Marce <101135405+Marce952@users.noreply.github.com>
2023-03-15 16:20:40 -0300

    Add files via upload

---
+++ Administrador/axiosAdmin.js
@@ -0,0 +1,56 @@
+const irAPrincipal = () =>{
+    let paginaPrincipal = "../Clases/Principal.html"
+    location.href = paginaPrincipal
+}
+
+const irAAlumnos = () =>{
+    let paginaAlumnos = "Alumnos.html"
+    location.href = paginaAlumnos
+}
+
+const salir = () =>{
+    let paginaIndex = "../index.html"
+    location.href = paginaIndex
+}
+
+const listadoDeAlumnos = () =>{
+
+    axios.get('http://localhost:3005/alumnos')
+        .then((resp) => {
+            let tdNombre = document.getElementById('tdNombre')
+            let tdApellido = document.getElementById('tdApellido')
+            let tdTarea1 = document.getElementById('tdTarea1')
+            let tdTarea2 = document.getElementById('tdTarea2')
+            let tdTarea3 = document.getElementById('tdTarea3')
+            let tdTarea4 = document.getElementById('tdTarea4')
+            
+            for(let i = 0; i < resp.data.length; i++){
+                const pNombre = document.createElement('p')
+                const pApellido = document.createElement('p')
+                const pDNI = document.createElement('p')
+                const pTarea1 = document.createElement('p')
+                const pTarea2 = document.createElement('p')
+                const pTarea3 = document.createElement('p')
+                const pTarea4 = document.createElement('p')
+
+                pNombre.textContent = resp.data[i].nombre
+                pApellido.textContent = resp.data[i].apellido
+                pDNI.textContent = resp.data[i].dni
+                pTarea1.textContent = resp.data[i].tarea1
+                pTarea2.textContent = resp.data[i].tarea2
+                pTarea3.textContent = resp.data[i].tarea3
+                pTarea4.textContent = resp.data[i].tarea4
+                
+                tdNombre.appendChild(pNombre)
+                tdApellido.appendChild(pApellido)
+                tdDNI.appendChild(pDNI)
+
+                tdTarea1.appendChild(pTarea1)
+                tdTarea2.appendChild(pTarea2)
+                tdTarea3.appendChild(pTarea3)
+                tdTarea4.appendChild(pTarea4)
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+}
