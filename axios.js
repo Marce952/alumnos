19de99ac646e8b9379ec32a990b9ac3d4240d594
Marce952 <101135405+Marce952@users.noreply.github.com>
2023-04-06 14:04:26 -0300

    New Host

--- axios.js
+++ axios.js
@@ -1,6 +1,8 @@
 let pagina = "Clases/Principal.html";
 let host = 'https://apialumnos.netlify.app/api'
 // let host = 'http://localhost:3005'
+// let host = 'http://localhost:3000/api'
+
 
 // ======== INDEX.HTML ==========
 
