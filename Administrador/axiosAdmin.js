05986d7bc1059a4e7092c0279c59b0749aa0643d
Marce952 <101135405+Marce952@users.noreply.github.com>
2023-06-14 15:48:28 -0300

    Alistando Clases y contenidos

--- Administrador/axiosAdmin.js
+++ Administrador/axiosAdmin.js
@@ -27,7 +27,7 @@ const listadoDeAlumnos = () => {
             let tdTarea3 = document.getElementById('tdTarea3')
             let tdTarea4 = document.getElementById('tdTarea4')
 
-            for (let i = 0; i < resp.data.length; i++) {
+            resp.data.forEach((alumno) => {
                 const pNombre = document.createElement('p')
                 const pApellido = document.createElement('p')
                 const pDNI = document.createElement('p')
@@ -35,29 +35,25 @@ const listadoDeAlumnos = () => {
                 const pTarea2 = document.createElement('p')
                 const pTarea3 = document.createElement('p')
                 const pTarea4 = document.createElement('p')
-
-                pNombre.textContent = resp.data[i].nombre
-                pApellido.textContent = resp.data[i].apellido
-                pDNI.textContent = resp.data[i].dni
-                pTarea1.textContent = resp.data[i].tarea1
-                pTarea2.textContent = resp.data[i].tarea2
-                pTarea3.textContent = resp.data[i].tarea3
-                pTarea4.textContent = resp.data[i].tarea4
-
+        
+                pNombre.textContent = alumno.nombre
+                pApellido.textContent = alumno.apellido
+                pDNI.textContent = alumno.dni
+        
+                pTarea1.textContent = alumno.tarea1 == 1 ? 'S' : 'N'
+                pTarea2.textContent = alumno.tarea2 == 1 ? 'S' : 'N'
+                pTarea3.textContent = alumno.tarea3 == 1 ? 'S' : 'N'
+                pTarea4.textContent = alumno.tarea4 == 1 ? 'S' : 'N'
+        
                 tdNombre.appendChild(pNombre)
                 tdApellido.appendChild(pApellido)
                 tdDNI.appendChild(pDNI)
-
-                pTarea1.textContent = resp.data[i].tarea1 == 1 ? 'S' : 'N'
-                pTarea2.textContent = resp.data[i].tarea2 == 1 ? 'S' : 'N'
-                pTarea3.textContent = resp.data[i].tarea3 == 1 ? 'S' : 'N'
-                pTarea4.textContent = resp.data[i].tarea4 == 1 ? 'S' : 'N'
-
+        
                 tdTarea1.appendChild(pTarea1)
                 tdTarea2.appendChild(pTarea2)
                 tdTarea3.appendChild(pTarea3)
                 tdTarea4.appendChild(pTarea4)
-            }
+              })
         }).catch((error) => {
             console.log(error)
         })
